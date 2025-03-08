4c6ea9ad4c94e064f821c44f9715576fc4ec5e45
Julio Resende <julioresende07@gmail.com>
2025-03-07 23:28:05 -0300

    Ajustes para o deploy

--- back-end-projeto-spotify/api/server.js
+++ back-end-projeto-spotify/api/server.js
@@ -1,22 +1,35 @@
 import express from "express";
 import cors from "cors";
 import { db } from "./connect.js";
+import path from "path";
+
+const __dirname = path.resolve();
 
 const app = express();
 const port = 3000;
 
 app.use(cors());
 
-app.get("/", (req, res) => {
+app.get("/api/", (req, res) => {
   res.send("Endpoints '/artists' & '/songs'");
 });
 
-app.get("/artists", async (req, res) => {
+app.get("/api/artists", async (req, res) => {
   res.send(await db.collection("artists").find({}).toArray());
 });
 
-app.get("/songs", async (req, res) => {
+app.get("/api/songs", async (req, res) => {
   res.send(await db.collection("songs").find({}).toArray());
+});
+
+app.use(
+  express.static(path.join(__dirname, "../front-end-projeto-spotify/dist"))
+);
+
+app.get("*", async (req, res) => {
+  res.sendFile(
+    path.join(__dirname, "../front-end-projeto-spotify/dist/index.html")
+  );
 });
 
 app.listen(port, () => {
--- front-end-projeto-spotify/api/api.js
+++ front-end-projeto-spotify/api/api.js
@@ -1,6 +1,8 @@
+import "dotenv/config";
 import axios from "axios";
 
-const url = "http://localhost:3000";
+const { NODE_ENV } = process.env;
+const url = NODE_ENV === "development" ? "http://localhost:3000/api" : "/api";
 
 // const api = axios.create({
 //   baseURL: `${url}`,
