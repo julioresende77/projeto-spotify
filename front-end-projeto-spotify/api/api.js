49a35ee97271aa176a2d527f9c9406814ea2e02f
Julio Resende <julioresende07@gmail.com>
2025-03-07 23:47:47 -0300

    Ajustes no process env para o deploy

--- front-end-projeto-spotify/api/api.js
+++ front-end-projeto-spotify/api/api.js
@@ -1,8 +1,10 @@
 import "dotenv/config";
 import axios from "axios";
 
-const NODE_ENV = process.env.NODE_ENV;
-const url = NODE_ENV === "development" ? "http://localhost:3000/api" : "/api";
+// const NODE_ENV = process.env.NODE_ENV;
+// const url = NODE_ENV === "development" ? "http://localhost:3000/api" : "/api";
+
+const url = "https://projeto-spotify-w88b.onrender.com/api";
 
 // const api = axios.create({
 //   baseURL: `${url}`,
