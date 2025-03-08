7777f934b973cd60ad37a4d900b7b661e747f661
Julio Resende <julioresende07@gmail.com>
2025-03-08 18:18:26 -0300

    Resolvido a exposição da URI

--- back-end-projeto-spotify/api/connect.js
+++ back-end-projeto-spotify/api/connect.js
@@ -1,10 +1,21 @@
-import { MongoClient } from "mongodb";
+import { MongoClient } from 'mongodb';
+import dotenv from 'dotenv'; // Importe dotenv
 
-const url = "mongodb+srv://juliooliver77:Resende77@cluster0.hxmzg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+dotenv.config(); // Carregue as variáveis de ambiente
 
+const url = process.env.MONGODB_URL;
 const client = new MongoClient(url);
 
 export const db = client.db("projeto-spotify");
+
+
+// import { MongoClient } from "mongodb";
+
+// const url = "mongodb+srv://juliooliver77:Resende77@cluster0.hxmzg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+// const client = new MongoClient(url);
+
+// export const db = client.db("projeto-spotify");
 
 // const artistCollection = await db.collection("artists").find({}).toArray();
 // const songCollection = await db.collection("songs").find({}).toArray();
